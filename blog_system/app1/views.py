278e4997ca4e9c50eaeebf2d313b765e1e2a0b10
anurk <anuradhakrishnanav2002@gmail.com>
2024-08-05 19:39:52 +0530

    changes

--- blog_system/app1/views.py
+++ blog_system/app1/views.py
@@ -333,6 +333,22 @@ class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 
+# class UserList(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     permission_classes = [IsAdminUser]
+
+#     def list(self, request):
+#         # Note the use of `get_queryset()` instead of `self.queryset`
+#         queryset = self.get_queryset()
+#         serializer = UserSerializer(queryset, many=True)
+#         return Response(serializer.data)
+
+    
+
+
+    
+
 
 
 
--- blog_system/blog_system/settings.py
+++ blog_system/blog_system/settings.py
@@ -156,11 +156,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 
+from decouple import config
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL= False
-EMAIL_HOST_USER = 'anuradhakrishnanav2002@gmail.com'  
-EMAIL_HOST_PASSWORD = 'gfrm wamh xblx wguv' 
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
